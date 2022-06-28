4bb7fb6c0feb457a2bcb097cb39ca6b7fd28700c
wmb6598 <wmb6598@gmail.com>
2022-06-28 09:23:17 +0100

    m

--- Learn Python by Building Five Games/Pong/Pong.py
+++ Learn Python by Building Five Games/Pong/Pong.py
@@ -16,6 +16,10 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
+# Score
+score_a = 0
+score_b = 0
+
 
 # Paddle B
 paddle_b = turtle.Turtle()
@@ -35,6 +39,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.1
 ball.dy = 0.1
+
+# Score / Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align = "center", font = ("Courier", 24, "normal"))
 
 
 # Functions
@@ -85,8 +98,22 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
 
     if ball.xcor() < - 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align = "center", font = ("Courier", 24, "normal"))
 
+    # Paddle and Ball colissions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *=-1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *=-1
